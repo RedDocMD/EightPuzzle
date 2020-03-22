17fa181b2e6cfb4e03e67faaec021b88431db8da
Deep Majumder <deep.majumder2019@gmail.com>
2020-03-22 15:32:49 +0530

    One constructor and copy constructor made

--- include/gameposition.hpp
+++ include/gameposition.hpp
@@ -8,15 +8,17 @@ public:
   explicit GamePosition(std::vector<std::vector<int>> board)
       : size{board.size()}, board{board}, next{} {}
   explicit GamePosition(unsigned size);
-  explicit GamePosition(const GamePosition &game);
-  explicit GamePosition(const GamePosition &&game);
-  // TODO: Copy and move operators
+  explicit GamePosition(const GamePosition &game)
+      : size{game.size}, board{game.board}, next{game.next} {}
+  explicit GamePosition(GamePosition &&game);
+  GamePosition &operator=(const GamePosition &other);
+  GamePosition &operator=(GamePosition &&other);
   bool operator==(const GamePosition &other) const;
   void eval_next();
   unsigned get_size() const { return size; }
 
 private:
-  unsigned size;
+  unsigned long size;
   std::vector<std::vector<int>> board;
   std::vector<std::shared_ptr<GamePosition>> next;
 };
--- src/gameposition.cpp
+++ src/gameposition.cpp
@@ -1,3 +1,32 @@
 #include "gameposition.hpp"
 
+#include <random>
+#include <unordered_set>
+
 using namespace std;
+
+GamePosition::GamePosition(unsigned size) {
+  this->size = size;
+  this->board = vector<vector<int>>(size, vector<int>(size, 0));
+  this->next = {};
+
+  default_random_engine dre{};
+  uniform_int_distribution<int> pos_gen(0, size * size - 1);
+  uniform_int_distribution<int> num_gen(1, size * size - 1);
+  unordered_set<int> done{};
+
+  auto pos{pos_gen(dre)};
+  auto posx{pos / size}, posy{pos % size};
+
+  for (auto i = 0U; i < size; i++) {
+    for (auto j = 0U; j < size; j++) {
+      if (!(i == posx && j == posy)) {
+        auto el{num_gen(dre)};
+        while (done.find(el) != done.end())
+          el = num_gen(dre);
+        this->board[i][j] = el;
+        done.insert(el);
+      }
+    }
+  }
+}
