7fe6c8de761fc3443f6b030b7ed940cd5981c41e
Deep Majumder <deep.majumder2019@gmail.com>
2020-03-23 22:16:13 +0530

    Some refactoring

--- main.cpp
+++ main.cpp
@@ -5,19 +5,20 @@
 using namespace std;
 
 int main(int argc, char const *argv[]) {
+  cout << "Eight puzzle" << endl;
   vector<vector<int>> goal_board_eight{{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
   vector<vector<int>> start_board_eight{{1, 0, 4}, {2, 5, 3}, {8, 7, 6}};
-  auto stash = make_unique<NodeStash>(NodeStash());
+  auto stash_eight = make_unique<NodeStash>(NodeStash());
 
-  auto start_position =
-      make_unique<GamePosition>(GamePosition(start_board_eight, stash.get()));
-  auto goal_position =
-      make_unique<GamePosition>(GamePosition(goal_board_eight, stash.get()));
+  auto start_position_eight = make_unique<GamePosition>(
+      GamePosition(start_board_eight, stash_eight.get()));
+  auto goal_position_eight = make_unique<GamePosition>(
+      GamePosition(goal_board_eight, stash_eight.get()));
 
-  stash->push(start_board_eight, start_position);
-  stash->push(goal_board_eight, goal_position);
-  auto path = astar_search(stash->get(start_board_eight).get(),
-                           stash->get(goal_board_eight).get());
-  cout << path;
+  stash_eight->push(start_board_eight, start_position_eight);
+  stash_eight->push(goal_board_eight, goal_position_eight);
+  auto path_eight = astar_search(stash_eight->get(start_board_eight).get(),
+                                 stash_eight->get(goal_board_eight).get());
+  cout << path_eight << endl;
   return 0;
 }
